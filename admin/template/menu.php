f67da1271fce87f5de2a6c3172119bff9700c1b0
Sachio17 <mfrmdhn88@gmail.com>
2026-04-28 09:59:35 +0700

    Add files via upload

---
+++ admin/template/menu.php
@@ -0,0 +1,384 @@
+<style>
+  
+  .app-sidebar {
+    background-color: #d4edda !important;
+}
+.app-sidebar .nav-link {
+    color: black !important;
+}
+.app-sidebar .nav-icon {
+    color: black !important;
+}
+
+</style>
+<!--begin::Sidebar-->
+      <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+        <!--begin::Sidebar Brand-->
+        <div class="sidebar-brand">
+          <!--begin::Brand Link-->
+          <a href="hal_admin.php" class="brand-link">
+            <!--begin::Brand Image-->
+           
+            <!--end::Brand Image-->
+            <!--begin::Brand Text-->
+      <h1 class="admin-title">Admin Sekolah</h1>
+
+<style>
+.admin-title {
+  font-size: 22px;
+  font-weight: 500;
+  color: #1f2937;
+}
+</style>
+
+            <!--end::Brand Text-->
+          </a>
+          <!--end::Brand Link-->
+        </div>
+        <!--end::Sidebar Brand-->
+        <!--begin::Sidebar Wrapper-->
+        <div class="sidebar-wrapper">
+          <nav class="mt-2">
+            <!--begin::Sidebar Menu-->
+            <ul
+              class="nav sidebar-menu flex-column"
+              data-lte-toggle="treeview"
+              role="navigation"
+              aria-label="Main navigation"
+              data-accordion="false"
+              id="navigation"
+            >
+              <li class="nav-item menu-open">
+                <a href="hal_admin.php" class="nav-link active">
+             <i class="bi bi-speedometer2"></i>
+                  <p>
+                    Dashboard
+                    
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    
+                  </li>
+                  
+                  
+                </ul>
+              </li>
+              
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="bi bi-newspaper"></i>
+                  <p>
+                    Berita
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+               
+
+ 
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="input_berita.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Input Berita</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="data_berita.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Data Berita</p>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </li>
+                <li class="nav-item">
+                <a href="#" class="nav-link">
+              <i class="bi bi-camera-video-fill"></i>
+                  <p>
+                    Video Kegiatan
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+               
+
+ 
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="input_video.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Input Video</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="data_video.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Data Video</p>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </li>
+
+              <li class="nav-item">
+  <a href="#" class="nav-link">
+    <i class="bi bi-building-fill-gear"></i>
+    <p>
+      Fasilitas
+      <i class="nav-arrow bi bi-chevron-right"></i>
+    </p>
+  </a>
+
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="input_fasilitas.php" class="nav-link">
+        <i class="nav-icon bi bi-circle"></i>
+        <p>Input Fasilitas</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="data_fasilitas.php" class="nav-link">
+        <i class="nav-icon bi bi-circle"></i>
+        <p>Data Fasilitas</p>
+      </a>
+    </li>
+  </ul>
+</li>
+
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="bi bi-card-image"></i>
+                  <p>
+                    Galeri
+                    
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                  <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="input_galeri.php" class="nav-link active">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Input Galeri</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="data_galeri.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Data Galeri</p>
+                    </a>
+                  </li>
+                 
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="bi bi-person-square"></i>
+                  <p>
+                    Guru
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="input_guru.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Input Guru</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="data_guru.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Data Guru</p>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+          <i class="bi bi-mortarboard"></i>
+                  <p>
+                   Jurusan
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="input_jurusan.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Input jurusan</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="data_jurusan.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Data jurusan</p>
+                    </a>
+                  </li>
+                  
+                </ul>
+               <li class="nav-item">
+                <a href="#" class="nav-link">
+          <i class="bi bi-trophy-fill"></i>
+                  <p>
+                   Ekstrakuriler
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="input_eskul.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Input eskul</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="data_eskul.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Data eskul</p>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </li>
+                <li class="nav-item">
+                <a href="#" class="nav-link">
+                 <i class="bi bi-clock"></i>
+                  <p>
+                    Jadwal Harian
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="input_jadwal.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Input Jadwal</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="data_jadwal.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Data Jadwal</p>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+              <i class="bi bi-envelope"></i>
+                  <p>
+                  Kontak
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="input_kontak.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Input Kontak</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="data_kontak.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Data Kontak</p>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </li>
+               <li class="nav-item">
+                <a href="#" class="nav-link">
+<i class="bi bi-clipboard"></i>                  <p>
+                  Pendaftaran
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                   
+                  </li>
+                  <li class="nav-item">
+                    <a href="data_pendaftaran.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Data Pendaftaran</p>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                 <i class="bi bi-chat-quote"></i>
+                  <p>
+                    Testimoni Alumni
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="input_testimoni.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Input Testimoni</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="data_testimoni.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Data Testimoni</p>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="bi bi-building"></i>
+                  <p>
+                    Sekolah
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                 <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="input_sekolah.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Input Sekolah</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="data_sekolah.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Data Sekolah</p>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="index.php" class="nav-link" onclick="return confirm('Anda yakin ingin logout ?')">
+
+                  <i class="bi bi-box-arrow-right"></i>
+
+                  <p>
+                    Logout
+                   
+                  </p>
+                </a>
+                
+                    
+                   
+                   
+                  </li>
+                </ul>
+              </li>
+              
+            <!--end::Sidebar Menu-->
+          </nav>
+        </div>
+        <!--end::Sidebar Wrapper-->
+      </aside>
+      <!--end::Sidebar-->
+      
